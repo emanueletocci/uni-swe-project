e73f7576bad5862b8546d7e1e5d0eb840480a1a2
Emanuele Tocci <emato03.felix@gmail.com>
2024-11-20 16:53:49 +0100

    project creation

---
+++ sources/Rubrica/src/main/java/it/unisa/diem/oop/group/rubrica/Rubrica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package it.unisa.diem.oop.group.rubrica;
+
+/**
+ *
+ * @author emanueletocci
+ */
+public class Rubrica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
