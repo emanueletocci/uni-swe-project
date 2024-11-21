4daadf052a364fb4639cb20df757acb55a835ef7
Emanuele Tocci <emato03.felix@gmail.com>
2024-11-21 12:44:07 +0100

    aggiornamento progetto maven

--- sources/Rubrica/src/main/java/it/unisa/diem/oop/group07/rubrica/Rubrica.java
+++ sources/Rubrica/src/main/java/it/unisa/diem/oop/group07/rubrica/Rubrica.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package it.unisa.diem.oop.group.rubrica;
+package it.unisa.diem.oop.group07.rubrica;
 
 /**
  *
