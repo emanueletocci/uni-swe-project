93e64a6513311eae4d692a6fbf861c1d028a4c8f
Emanuele Tocci <emato03.felix@gmail.com>
2024-12-03 16:33:24 +0100

    Aggiornamento diagramma classi

--- sources/Rubrica/src/main/java/it/unisa/diem/oop/group07/rubrica/Rubrica.java
+++ sources/Rubrica/src/main/java/it/unisa/diem/oop/group07/rubrica/Rubrica.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 public class Rubrica {
     Map rubrica;
-
     public void aggiungiContatto(Contatto c){}
     public Contatto rimuoviContatto(Contatto c){}
     public Contatto ricercaContatto(Contatto c){}
