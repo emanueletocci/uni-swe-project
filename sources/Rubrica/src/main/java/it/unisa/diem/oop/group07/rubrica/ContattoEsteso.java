2df58b655f5e6cdd4f772c1142aba074d01fc0b4
ClaudiaMontefusco3 <c.montefusco6@studenti.unisa.it>
2024-12-04 17:17:00 +0100

    aggiunta commenti doxygen

--- sources/Rubrica/src/main/java/it/unisa/diem/oop/group07/rubrica/ContattoEsteso.java
+++ sources/Rubrica/src/main/java/it/unisa/diem/oop/group07/rubrica/ContattoEsteso.java
@@ -1,9 +1,25 @@
-package it.unisa.diem.oop.group07.rubrica;
 
+
+/**
+ * @file ContattoEsteso.java
+ * @brief questo file aggiunge informazioni aggiuntive al Contatto: compeanno, indirizzo di residenza e socialLinks.
+ * @autor Gruppo07
+ * @date Dicembre, 2024
+ * @version 1.0
+ */
+package it.unisa.diem.oop.group07.rubrica;
 public class ContattoEsteso extends Contatto{
+    ///@brief data di compleanno del contatto esteso
     private LocalDate compleanno;
+     ///@brief indirizzo di residenza del contatto esteso
     private String indirizzoResidenza;
+     ///@brief link relativo al social del contatto esteso
     private String socialLinks;
+
+
+    /**
+     * @brief costruttore della classe Contatto Esteso
+     */
 
     public ContattoEsteso(String nome, String cognome, String[] emails, int[] numeri, Boolean emergenza, Boolean preferiti, LocalDate compleanno, String indirizzoResidenza, String socialLinks) {
         super(nome, cognome, emails, numeri, emergenza, preferiti);
@@ -12,26 +28,45 @@ public class ContattoEsteso extends Contatto{
         this.socialLinks = socialLinks;
     }
 
+
+     /**
+     * @brief metodo getter per l'attributo "compleanno"
+     * @return attributo compleanno
+     */
+
     public LocalDate getCompleanno() {
         return compleanno;
-    }
+    /**
+     * @brief metodo setter per l'attributo "compleanno"
+     */
+
 
     public void setCompleanno(LocalDate compleanno) {
         this.compleanno = compleanno;
     }
-
+    /**
+     * @brief metodo getter per l'attributo "indirizzoResidenza"
+     * @return attributo indirizzoResidenza
+     */
     public String getIndirizzoResidenza() {
         return indirizzoResidenza;
     }
-
+    /**
+     * @brief metodo setter per l'attributo "indirizzoResidenza"
+     */
     public void setIndirizzoResidenza(String indirizzoResidenza) {
         this.indirizzoResidenza = indirizzoResidenza;
     }
-
+    /**
+     * @brief metodo getter per l'attributo "socialLinks"
+     * @return attributo indirizzoResidenza
+     */
     public String getSocialLinks() {
         return socialLinks;
     }
-
+        /**
+     * @brief metodo setter per l'attributo "socialLinks"
+     */
     public void setSocialLinks(String socialLinks) {
         this.socialLinks = socialLinks;
     }
