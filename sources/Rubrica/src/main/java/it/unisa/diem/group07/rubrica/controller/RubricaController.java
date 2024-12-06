a358c75e5ffb639396498c8e93fdd5cd4d87c8bc
ClaudiaMontefusco3 <c.montefusco6@studenti.unisa.it>
2024-12-06 09:16:20 +0100

    commento doxygen Rubrica Controller

--- sources/Rubrica/src/main/java/it/unisa/diem/group07/rubrica/controller/RubricaController.java
+++ sources/Rubrica/src/main/java/it/unisa/diem/group07/rubrica/controller/RubricaController.java
@@ -1,4 +1,10 @@
 package it.unisa.diem.group07.rubrica.controller;
-
+/**
+ * @file RubricaController.java
+ * @brief questo file contiene l'implementazione della classe RubricaController per la gestione generale della Rubrica.
+ * @autor Gruppo07
+ * @date Dicembre, 2024
+ * @version 1.0
+ */
 public class RubricaController extends MainController{
 }
