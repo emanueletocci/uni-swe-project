347f8232ec8c168f78291307cb8e8c4b25b9fa07
ClaudiaMontefusco3 <c.montefusco6@studenti.unisa.it>
2024-12-06 09:08:16 +0100

    commento doxygen MainController

--- sources/Rubrica/src/main/java/it/unisa/diem/group07/rubrica/controller/MainController.java
+++ sources/Rubrica/src/main/java/it/unisa/diem/group07/rubrica/controller/MainController.java
@@ -1,4 +1,12 @@
 package it.unisa.diem.group07.rubrica.controller;
+/**
+ * @file Contatto.java
+ * @brief questo file contiene l'implementazione del Controller principale per la gestione della rubrica telefonica.
+ *        questa classe gestisce l'interazione tra l'utente e il modello di dati della rubrica.
+ *  @autor Gruppo07
+ * @date Dicembre, 2024
+ * @version 1.0
+ */
 
 public class MainController {
 }
