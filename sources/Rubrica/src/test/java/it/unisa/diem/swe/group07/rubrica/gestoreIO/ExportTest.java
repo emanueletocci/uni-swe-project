dacb5b355b8a6beb215a6198d484995401adbde5
ClaudiaMontefusco3 <c.montefusco6@studenti.unisa.it>
2024-12-11 21:17:20 +0100

    creazione classe ExportTest.java

---
+++ sources/Rubrica/src/test/java/it/unisa/diem/swe/group07/rubrica/gestoreIO/ExportTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.swe.group07.rubrica.gestoreIO;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author claud
+ */
+public class ExportTest {
+    
+    public ExportTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+}
