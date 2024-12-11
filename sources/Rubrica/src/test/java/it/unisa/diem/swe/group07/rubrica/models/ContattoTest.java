5bd49a88db7fc4b0480b2046fd86bc6f590fc038
ClaudiaMontefusco3 <c.montefusco6@studenti.unisa.it>
2024-12-11 18:27:56 +0100

    implementazione ContattoTest.java (da completare)

--- sources/Rubrica/src/test/java/it/unisa/diem/swe/group07/rubrica/models/ContattoTest.java
+++ sources/Rubrica/src/test/java/it/unisa/diem/swe/group07/rubrica/models/ContattoTest.java
@@ -5,36 +5,38 @@
  */
 package it.unisa.diem.swe.group07.rubrica.models;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
  * @author claud
  */
 public class ContattoTest {
+       
+  @Test
+    public void testConstructor() {
+      Contatto c = new Contatto("Rossella", "Pale", "+3933333333", "+3933333333", "+3933333333", true, true);
+      assertEquals("Rossella", c.getNome());
+      assertEquals("Pale", c.getCogome());
+      assertEquals("+3933333333", c.getTelefono1());
+      assertEquals("+3933333333", c.getTelefono2());
+      assertEquals("+3933333333", c.getTelefono3());
+      assertTrue(c.getPreferito());
+      assertFalse(c.getEmergenza());
+      
+      }
     
-    public ContattoTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-    
+@Test 
+public void testGetterESetter() {
+      Contatto c = new Contatto("Alessio", "Leo", "+3933333333", "+3933333333", "+3933333333", false, false);
+      assertEquals("Alessio", c.getNome());
+      assertEquals("Lep", c.getCogome());
+      assertEquals("+3933333333", c.getTelefono1());
+      assertEquals("+3933333333", c.getTelefono2());
+      assertEquals("+3933333333", c.getTelefono3());
+      assertTrue(c.getPreferito());
+      assertFalse(c.getEmergenza());
+      
+}
 }
