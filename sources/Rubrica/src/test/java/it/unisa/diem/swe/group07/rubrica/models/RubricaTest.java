17e7db16590e7cd3dff5792869988fcd871c38a8
Alessio Leo <a.leo88@studenti.unisa.it>
2024-12-12 12:09:35 +0100

    test della classe rubrica tutti completati con successo

--- sources/Rubrica/src/test/java/it/unisa/diem/swe/group07/rubrica/models/RubricaTest.java
+++ sources/Rubrica/src/test/java/it/unisa/diem/swe/group07/rubrica/models/RubricaTest.java
@@ -1,52 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.diem.swe.group07.rubrica.models;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RubricaTest {
 
+    private ContattoEsteso c;
+    private Rubrica r;
+
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void aggiungiContatto() {
+        c = new ContattoEsteso("nome", "cognome", "num1", "num2", "num3", "em1", "em2", "em3", LocalDate.of(1970, 1, 1), "adr", "link", "note", true, true);
+        r = new Rubrica();
     }
 
     @Test
-    void aggiungiContattoEVerifica() {
+    void TestAggiungiContatto() {
+        assertNull(r.ricercaContatto(c));
+        r.aggiungiContatto(c);
+        assertEquals(c, r.ricercaContatto(c));
     }
 
     @Test
-    void rimuoviContatto() {
+    void TestAggiungiContattoEVerifica() {
+        assertTrue(r.aggiungiContattoEVerifica(c));
+        assertFalse(r.aggiungiContattoEVerifica(c));
     }
 
     @Test
-    void ricercaContatto() {
+    void TestRimuoviContatto() {
+        assertNull(r.rimuoviContatto(c));
+        r.aggiungiContatto(c);
+        assertEquals(c, r.rimuoviContatto(c));
     }
 
     @Test
-    void getContatti() {
+    void TestRicercaContatto() {
+        assertNull(r.ricercaContatto(c));
+        r.aggiungiContatto(c);
+        assertEquals(c, r.ricercaContatto(c));
     }
 
     @Test
-    void aggiornaContatto() {
+    void TestGetContatti() {
+        assertFalse(r.getContatti().contains(c));
+        r.aggiungiContatto(c);
+        assertTrue(r.getContatti().contains(c));
     }
 
     @Test
-    void testToString() {
+    void TestAggiornaContatto() {
+        assertFalse(r.aggiornaContatto(c));
+        r.aggiungiContatto(c);
+        assertTrue(r.aggiornaContatto(c));
+        assertEquals("nome", c.getNome());
+        c.setNome("nome2");
+        r.aggiornaContatto(c);
+        assertEquals("nome2", r.ricercaContatto(c).getNome());
+    }
+
+    @Test
+    void TestToString() {
+        r.aggiungiContatto(c);
+        String s = "\nRubrica\n" + r.getContatti();
+        assertEquals(s, r.toString());
+
     }
 }
