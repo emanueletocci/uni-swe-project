e0241cffaa631fba364ba466068e60a3a2ccc4d0
emanueletocci <tocciemanuele03@gmail.com>
2024-12-13 22:26:31 +0100

    preparazione al merge

--- sources/Rubrica/src/main/java/it/unisa/diem/group07/rubrica/controller/RubricaController.java
+++
@@ -1,4 +0,0 @@
-package it.unisa.diem.group07.rubrica.controller;
-
-public class RubricaController extends MainController{
-}
--- sources/Rubrica/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module it.unisa.diem.rubrica.rubrica {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens it.unisa.diem.group07.rubrica to javafx.fxml;
-    exports it.unisa.diem.group07.rubrica;
-}
