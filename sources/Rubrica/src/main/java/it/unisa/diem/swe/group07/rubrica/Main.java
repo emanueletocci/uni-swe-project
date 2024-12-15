8fe8a73c548728efa0921f9715b906bdf9ed8a0e
emanueletocci <tocciemanuele03@gmail.com>
2024-12-15 15:38:00 +0100

    Aggiornamenti vari (.jarfiles)

---
+++ sources/Rubrica/src/main/java/it/unisa/diem/swe/group07/rubrica/Main.java
@@ -0,0 +1,11 @@
+package it.unisa.diem.swe.group07.rubrica;
+
+/**
+ * Classe usata esclusivamente dal plugin shade per la generazione del file .jar dato che non é possibile utilizzare
+ * direttamente una classe che estende "Application".
+ */
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
